6113a96749ba472993cee3bcb78d1dec0d13d8cf
Venca277 <venca03@centrum.cz>
2025-07-27 21:40:58 +0200

    inheritance of Kryptonform, testing performance

--- STK2/Form1.Designer.cs
+++ STK2/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(715, 512);
+            this.ClientSize = new System.Drawing.Size(648, 449);
             this.Name = "Form1";
             this.Text = "Přihlašte se";
             this.ResumeLayout(false);
