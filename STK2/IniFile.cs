89b12b847c4a4034e1ff726bb6c5951504bcffce
Venca277 <venca03@centrum.cz>
2025-07-28 11:51:12 +0200

    inifile class declaration for config files

---
+++ STK2/IniFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace STK2
+{
+    internal class IniFile
+    {
+        private string path;
+
+        public IniFile(string path)
+        {
+            this.path = path;
+
+        }
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        private static extern int GetPrivateProfileString(string section, string key, string defaultValue, StringBuilder retVal, int size, string filePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        private static extern bool WritePrivateProfileString(string section, string key, string value, string filePath);
+
+        public string Read(string section, string key, string defaultValue = "") { 
+            var value = new StringBuilder(255);
+            GetPrivateProfileString(section, key, defaultValue, value, 255, path);
+            return value.ToString();
+        }
+
+        public void Write(string section, string key, string value)
+        {
+            WritePrivateProfileString(section, key, value, path);
+        }
+    }
+
+}
+
